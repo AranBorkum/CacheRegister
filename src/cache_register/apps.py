1637cfb755386961e19f679e408a98a7d8002b42
Aran Borkum <aran.borkum@kraken.tech>
2025-02-23 10:48:29 +0000

    Load registers after AppConfig ready

--- src/cache_register/apps.py
+++ src/cache_register/apps.py
@@ -10,15 +10,15 @@ class CacheRegisterConfig(AppConfig):  # type: ignore [misc]
     default_auto_field = "django.db.models.BigAutoField"
     name = "cache_register"
 
+    def ready(self) -> None:
+        for app in settings.INSTALLED_APPS:
+            try:
+                importlib.import_module(f"{app}.registers")
+            except ModuleNotFoundError:
+                pass
 
-for app in settings.INSTALLED_APPS:
-    try:
-        importlib.import_module(f"{app}.registers")
-    except ModuleNotFoundError:
-        pass
-
-    for register in _global_dict_register:
-        try:
-            importlib.import_module(f"{app}.{register}")
-        except ModuleNotFoundError:
-            pass
+            for register in _global_dict_register:
+                try:
+                    importlib.import_module(f"{app}.{register}")
+                except ModuleNotFoundError:
+                    pass
